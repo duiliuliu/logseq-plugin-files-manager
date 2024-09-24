0e9efa5e8b83a7156af0e1315392e66b7afc11c8
wuruipeng.liuliu <wuruipeng.liuliu@bytedance.com>
2024-09-24 17:25:32 +0800

    Update logger.ts

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -6,6 +6,6 @@ const isDebug = __DEBUG__;
 console.log('DEBUG:', isDebug);
 // Default severities are: 0: silly, 1: trace, 2: debug, 3: info, 4: warn, 5: error, 6: fatal
 export const logger = new Logger({
-    // minLevel: isDebug ? 0 : 3,
-    minLevel: isDebug
+    minLevel: isDebug ? 0 : 3,
+    // minLevel: isDebug
 });
